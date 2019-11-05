692393e03935011873587e2a334a77406ae72e69
FireWolf16 <Patrick.laranjo@gmail.com>
2019-11-05 10:07:25 +0100

    Opdracht 4 is af

--- index.php
+++ index.php
@@ -2,58 +2,58 @@
 <html lang="en">
 
 <head>
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <link href="https://fonts.googleapis.com/css?family=Bangers" rel="stylesheet">
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="css/main.css">
-    <title>Monkey Business</title>
-
+  <link rel="stylesheet" type="text/css" href="style.css" />
+  <link href="https://fonts.googleapis.com/css?family=Bangers" rel="stylesheet" />
+  <meta charset="UTF-8" />
+  <link rel="stylesheet" href="css/main.css" />
+  <title>Monkey Business</title>
 </head>
 
 <body>
-
-    <div>
-        <img src="img/monkey-business.jpg" alt="Monkey Logo" class="center">
-    </div>
-    <div class="Header">
-        <h1>Select your monkey!</h1>
-    </div>
-    <div>
-        <img src="img/monkey_swings.png" alt="Monkey divider" class="center">
-    </div>
-    <a class="Links" href="https://www.google.nl/search?q=Baviaan&tbm=isch">Baviaan</a>
-    <br>
-    <a class="Links" href="https://www.google.nl/search?q=Guereza&tbm=isch">Guereza</a>
-    <br>
-    <a class="Links" href="https://www.google.nl/search?q=Langoer&tbm=isch">Langoer</a>
-    <br>
-    <a class="Links" href="https://www.google.nl/search?q=Neusaap&tbm=isch">Neusaap</a>
-    <br>
-    <a class="Links" href="https://www.google.nl/search?q=Tamarin&tbm=isch">Tamarin</a>
-    <br>
-    <a class="Links" href="https://www.google.nl/search?q=Brulaap&tbm=isch">Brulaap</a>
-    <br>
-    <a class="Links" href="https://www.google.nl/search?q=Halfaap&tbm=isch">Halfaap</a>
-    <br>
-    <a class="Links" href="https://www.google.nl/search?q=Mandril&tbm=isch">Mandril</a>
-    <br>
-    <a class="Links" href="https://www.google.nl/search?q=Oeakari&tbm=isch">Oeakari</a>
-    <br>
-    <a class="Links" href="https://www.google.nl/search?q=Faunaap&tbm=isch">Faunaap</a>
-    <br>
-    <a class="Links" href="https://www.google.nl/search?q=Hoelman&tbm=isch">Hoelman</a>
-    <br>
-    <a class="Links" href="https://www.google.nl/search?q=Meerkat&tbm=isch">Meerkat</a>
-    <br>
-    <a class="Links" href="https://www.google.nl/search?q=Oormaki&tbm=isch">Oormaki</a>
-    <br>
-    <a class="Links" href="https://www.google.nl/search?q=Gorilla&tbm=isch">Gorilla</a>
-    <br>
-    <a class="Links" href="https://www.google.nl/search?q=Kuifaap&tbm=isch">Kuifaap</a>
-    <br>
-    <a class="Links" href="https://www.google.nl/search?q=Mensaap&tbm=isch">Mensaap</a>
-    <br>
-    <a class="Links" href="https://www.google.nl/search?q=Spinaap&tbm=isch">Spinaap</a>
+  <div>
+    <img src="img/monkey-business.jpg" alt="Monkey Logo" class="center" />
+  </div>
+  <div class="Header">
+    <h1>Select your monkey!</h1>
+  </div>
+  <div>
+    <img src="img/monkey_swings.png" alt="Monkey divider" class="center" />
+  </div>
+  <a class="Links" href="https://www.google.nl/search?q=Baviaan&tbm=isch">Baviaan</a>
+  <br />
+  <a class="Links" href="https://www.google.nl/search?q=Guereza&tbm=isch">Guereza</a>
+  <br />
+  <a class="Links" href="https://www.google.nl/search?q=Langoer&tbm=isch">Langoer</a>
+  <br />
+  <a class="Links" href="https://www.google.nl/search?q=Neusaap&tbm=isch">Neusaap</a>
+  <br />
+  <a class="Links" href="https://www.google.nl/search?q=Tamarin&tbm=isch">Tamarin</a>
+  <br />
+  <a class="Links" href="https://www.google.nl/search?q=Brulaap&tbm=isch">Brulaap</a>
+  <br />
+  <a class="Links" href="https://www.google.nl/search?q=Halfaap&tbm=isch">Halfaap</a>
+  <br />
+  <a class="Links" href="https://www.google.nl/search?q=Mandril&tbm=isch">Mandril</a>
+  <br />
+  <a class="Links" href="https://www.google.nl/search?q=Oeakari&tbm=isch">Oeakari</a>
+  <br />
+  <a class="Links" href="https://www.google.nl/search?q=Faunaap&tbm=isch">Faunaap</a>
+  <br />
+  <a class="Links" href="https://www.google.nl/search?q=Hoelman&tbm=isch">Hoelman</a>
+  <br />
+  <a class="Links" href="https://www.google.nl/search?q=Meerkat&tbm=isch">Meerkat</a>
+  <br />
+  <a class="Links" href="https://www.google.nl/search?q=Oormaki&tbm=isch">Oormaki</a>
+  <br />
+  <a class="Links" href="https://www.google.nl/search?q=Gorilla&tbm=isch">Gorilla</a>
+  <br />
+  <a class="Links" href="https://www.google.nl/search?q=Kuifaap&tbm=isch">Kuifaap</a>
+  <br />
+  <a class="Links" href="https://www.google.nl/search?q=Mensaap&tbm=isch">Mensaap</a>
+  <br />
+  <a class="Links" href="https://www.google.nl/search?q=Spinaap&tbm=isch">Spinaap</a>
+  <br />
+  <a href="php/config.php">database</a>
 </body>
 
 </html>
